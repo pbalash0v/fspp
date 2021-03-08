1dafb98a464564c3cd98bca3e041848fc1a4bb96
Pavel Balashov <pbalashov@pm.me>
2021-03-08 17:28:17 +0300

    Add mod_event_socket and mod_commands

--- src/fs_cfg.hpp
+++ src/fs_cfg.hpp
@@ -42,56 +42,105 @@ inline void generate_fs_config(const std::filesystem::path& cfg_file_path)
 	configuration_pt.add("<xmlattr>.name", "configuration");
 	auto& conf_section = document.add_child("section", configuration_pt);
 
-	//--- configuration -> modules
-	auto modules = [](auto& parent_pt)
+	//--- configuration -> modules.conf
+	auto modules_ = [](auto& parent_pt)
 	{
-		bpt::ptree modules_pt;
-		modules_pt.add("<xmlattr>.name", "modules.conf");
-		auto& modules_conf = parent_pt.add_child("configuration", modules_pt);
+		bpt::ptree pt_;
+		pt_.add("<xmlattr>.name", "modules.conf");
+		auto& conf_ = parent_pt.add_child("configuration", pt_);
 
-		auto& modules = modules_conf.put("modules", "");
+		auto& modules = conf_.put("modules", "");
+
 		bpt::ptree mod_console_pt;
 		mod_console_pt.add("<xmlattr>.module", "mod_console");
 		modules.add_child("load", mod_console_pt);
+
 		bpt::ptree mod_logfile_pt;
 		mod_logfile_pt.add("<xmlattr>.module", "mod_logfile");
 		modules.add_child("load", mod_logfile_pt);
 
-		return modules_pt;
+		bpt::ptree mod_event_socket_pt;
+		mod_event_socket_pt.add("<xmlattr>.module", "mod_event_socket");
+		modules.add_child("load", mod_event_socket_pt);
+
+		bpt::ptree mod_commands_pt;
+		mod_commands_pt.add("<xmlattr>.module", "mod_commands");
+		modules.add_child("load", mod_commands_pt);
+
+		return pt_;
 	}(conf_section);
 
-	//--- configuration -> console
-	auto console = [&name_value_pt](auto& parent_pt)
+	//--- configuration -> console.conf
+	auto console_ = [&name_value_pt](auto& parent_pt)
 	{
-		bpt::ptree console_pt;
-		console_pt.add("<xmlattr>.name", "console.conf");
-		auto& console_conf = parent_pt.add_child("configuration", console_pt);
+		bpt::ptree pt_;
+		pt_.add("<xmlattr>.name", "console.conf");
+		auto& conf_ = parent_pt.add_child("configuration", pt_);
 
-		auto& mappings = console_conf.put("mappings", "");
+		auto& mappings = conf_.put("mappings", "");
 		mappings.add_child("map", name_value_pt("all", "console,debug,info,notice,warning,err,crit,alert"));
 
-		auto& settings = console_conf.put("settings", "");
+		auto& settings = conf_.put("settings", "");
 		settings.add_child("param", name_value_pt("colorize", "true"));
 		settings.add_child("param", name_value_pt("loglevel", "debug"));
 
-		return console_pt;
+		return pt_;
 	}(conf_section);
 
-	//--- configuration -> logfile
-	auto logfile = [&name_value_pt](auto& parent_pt)
+	//--- configuration -> logfile.conf
+	// TODO
+	auto logfile_ = [&name_value_pt](auto& parent_pt)
 	{
-		bpt::ptree logfile_pt;
-		logfile_pt.add("<xmlattr>.name", "logfile.conf");
-		auto& logfile_conf = parent_pt.add_child("configuration", logfile_pt);
+		bpt::ptree pt_;
+		pt_.add("<xmlattr>.name", "logfile.conf");
+		auto& conf_ = parent_pt.add_child("configuration", pt_);
 
-		auto& settings = logfile_conf.put("settings", "");
+		auto& settings = conf_.put("settings", "");
 		settings.add_child("param", name_value_pt("rotate-on-hup", "true"));
 
-		return logfile_pt;
+		return pt_;
+	}(conf_section);
+
+	//--- configuration -> switch.conf
+	auto switch_ = [&name_value_pt](auto& parent_pt)
+	{
+		bpt::ptree pt_;
+		pt_.add("<xmlattr>.name", "switch.conf");
+		auto& conf_ = parent_pt.add_child("configuration", pt_);
+
+		auto& settings = conf_.put("settings", "");
+		settings.add_child("param", name_value_pt("colorize-console", "true"));
+		settings.add_child("param", name_value_pt("dialplan-timestamps", "false"));
+		settings.add_child("param", name_value_pt("max-db-handles", "50"));
+		settings.add_child("param", name_value_pt("db-handle-timeout", "10"));
+		settings.add_child("param", name_value_pt("max-sessions", "1000"));
+		settings.add_child("param", name_value_pt("sessions-per-second", "30"));
+		settings.add_child("param", name_value_pt("loglevel", "debug"));
+		settings.add_child("param", name_value_pt("dump-cores", "yes"));
+
+		return pt_;
+	}(conf_section);
+
+	//--- configuration -> event_socket.conf
+	auto event_socket_ = [&name_value_pt](auto& parent_pt)
+	{
+		bpt::ptree pt_;
+		pt_.add("<xmlattr>.name", "event_socket.conf");
+		auto& conf_ = parent_pt.add_child("configuration", pt_);
+
+		auto& settings = conf_.put("settings", "");
+		settings.add_child("param", name_value_pt("nat-map", "false"));
+		settings.add_child("param", name_value_pt("listen-ip", "::"));
+		settings.add_child("param", name_value_pt("listen-port", "8021"));
+		settings.add_child("param", name_value_pt("password", "ClueCon"));
+
+		return pt_;
 	}(conf_section);
 
 
+	//
 	//--- dialplan
+	//
 	bpt::ptree dialplan_pt;
 	dialplan_pt.add("<xmlattr>.name", "dialplan");
 	document.add_child("section", dialplan_pt);
