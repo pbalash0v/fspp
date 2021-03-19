37ca44b7683b88ecd59feb2c176a0feb2748c4d4
Pavel Balashov <pbalashov@pm.me>
2021-03-20 00:27:44 +0300

    Add python module draft

---
+++ src/python/module.cpp
@@ -0,0 +1,14 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+
+#include <fspp.hpp>
+
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pyfspp, m)
+{
+    py::class_<fspp::lib, std::shared_ptr<fspp::lib>>(m, "FSPP")
+    	.def(py::init([]() { return std::make_shared<fspp::lib>(); }))
+		.def("__call__", &fspp::lib::operator());
+}
